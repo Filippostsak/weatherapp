70923b7ad287ec8f5e2d1796af41e5a1b12d0ac3
Filippostsak <filippostsak@gmail.com>
2024-04-15 00:36:48 +0300

    utils

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 A complete back-end application with nodeJS and MongoDB
 
+#02 Creating the util folder with forecast and geocode
+Also create the functionallity to write in console which country and city we want to display data.
+
 # 01 Creating the services-controllers and routes
 
 # 00 Creating the Server and add the API request.
--- server.js
+++ server.js
@@ -2,8 +2,32 @@ const express = require("express");
 require("dotenv").config();
 const weatherRoutes = require("./routes/weatherRoutes");
 const mapboxRoutes = require("./routes/mapboxRoutes");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 const app = express();
 const port = 3000;
+
+const address = process.argv[2];
+if (!address) {
+  return console.error("Please provide an address");
+} else {
+  geocode(address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return console.error("Error:", error);
+    }
+    console.log("Geocode Data:", { latitude, longitude, location });
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return console.error("Error:", error);
+      }
+      // Display the forecast (description, temperature, location, and percentage of rain chance)
+      console.log(location);
+      console.log(
+        `${forecastData.description}. It is currently ${forecastData.temperature} degrees out. There is a ${forecastData.precipitation}% chance of rain.`
+      );
+    });
+  });
+}
 
 app.use("/weather", weatherRoutes);
 app.use("/location", mapboxRoutes);
